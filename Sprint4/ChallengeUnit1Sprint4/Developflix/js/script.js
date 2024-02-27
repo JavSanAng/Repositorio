47afe4a77a53dff12ec899d1860575361138c371
Javier <javsanang@gmail.com>
2024-02-27 12:47:36 +0100

    Challenge terminado

--- Sprint4/ChallengeUnit1Sprint4/Developflix/js/script.js
+++ Sprint4/ChallengeUnit1Sprint4/Developflix/js/script.js
@@ -1,2 +1,39 @@
-import peliculas from './peliculas.js'
+import peliculas from "./peliculas.js";
+const linkImage = 'https://image.tmdb.org/t/p/w500';
 
+const films = (section, films) => {
+    films.forEach(pelicula => {
+        // Creo el div donde guardo la info de las pelis
+        const divFilm = document.createElement("div");
+
+        // Creo el img para meter la portada de la peli
+        const imgFilm = document.createElement("img");
+        imgFilm.src = `${linkImage}${pelicula.poster_path}`; // Usar poster_path para la imagen
+
+        // Creo el H3 para meter los títulos
+        const title = document.createElement("h3");
+        title.textContent = pelicula.title;
+
+        // Meto las movidas en el div 
+        divFilm.appendChild(imgFilm);
+        divFilm.appendChild(title);
+
+        // Meto los div en los section:
+        section.appendChild(divFilm);
+    });
+};
+
+// Crear Secciones Peliculas
+const sectionAction = document.getElementById('genero-28'); 
+const sectionThriller = document.getElementById('genero-53');
+const sectionAdventure = document.getElementById('genero-12');
+
+// Filtrar películas y mostrarlas en las secciones correspondientes
+const arrayThriller = peliculas.filter(pelicula => pelicula.genre_ids.includes(53));
+films(sectionThriller, arrayThriller);
+
+const arrayAction = peliculas.filter(pelicula => pelicula.genre_ids.includes(28));
+films(sectionAction, arrayAction);
+
+const arrayAdventure = peliculas.filter(pelicula => pelicula.genre_ids.includes(12));
+films(sectionAdventure, arrayAdventure);
